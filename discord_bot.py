ec0e35439dc5ebd3796532701a5b04514d3dd803
netdownload <netdownload@yandex.ru>
2020-06-08 11:23:49 +0300

    test hello message and send file

--- discord_bot.py
+++ discord_bot.py
@@ -1,3 +1,22 @@
 import discord
-from discord.ext import commands
 import secret
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello!')
+        await message.channel.send(file=discord.File('D:\PyCharmProject\Serial\Electro\demo.xlsx'))
+
+
+client.run(secret.DISCORD_TOKEN)
